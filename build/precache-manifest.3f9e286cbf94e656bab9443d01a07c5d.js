73aa713b785aea8b849565454ace11824bfab20f
James Oddy <jamesoddy8@gmail.com>
2021-07-21 23:49:13 +0100

    new build

---
+++ build/precache-manifest.3f9e286cbf94e656bab9443d01a07c5d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e77f5b4ee6ffd49acb53952c80470e54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e02d9384195fcb1656c3",
+    "url": "/static/js/2.1743e18c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1743e18c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9724cade377da1fa1bf",
+    "url": "/static/js/main.c1a93217.chunk.js"
+  },
+  {
+    "revision": "8a82b5748f18b7586a42",
+    "url": "/static/js/runtime-main.5aec0757.js"
+  },
+  {
+    "revision": "99128235c8c436b5b26fabd7593b8ee3",
+    "url": "/static/media/svg1.99128235.svg"
+  },
+  {
+    "revision": "34b69e62775e2a3fbcbb4aafc596e761",
+    "url": "/static/media/svg2.34b69e62.svg"
+  },
+  {
+    "revision": "b765a40b4c721cd32453e43ef3237d3b",
+    "url": "/static/media/svg3.b765a40b.svg"
+  },
+  {
+    "revision": "56dd5fa6b8d8db051286205e9b300951",
+    "url": "/static/media/svg4.56dd5fa6.svg"
+  }
+]);
